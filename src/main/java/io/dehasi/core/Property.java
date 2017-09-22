02a30398e528f1b2c89aa649be9c5cc094da337e
Ravil Galeyev <dehasi@yandex.ru>
2017-09-22 22:11:08 +0200

    Extract domain

--- src/main/java/io/dehasi/core/Property.java
+++ src/main/java/io/dehasi/core/Property.java
@@ -4,7 +4,6 @@ import com.google.common.base.MoreObjects;
 import com.google.common.base.Objects;
 import com.google.common.collect.ComparisonChain;
 import com.google.common.collect.Ordering;
-
 import javax.annotation.Nonnull;
 
 import java.util.List;
